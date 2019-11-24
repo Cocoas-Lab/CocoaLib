7283ee88259ce62d4e6492fa654436f3e4d244a3
Keita <koron0902@gmail.com>
2019-11-25 00:13:16 +0900

    create source file for test code

--- CocoaLib/src/CocoaLib.cpp
+++ CocoaLib/src/CocoaLib.cpp
@@ -7,25 +7,11 @@
  Description : main definition
 ===============================================================================
 */
-
-#include "driver/comm/uart.hpp"
-
-// TODO: insert other include files here
-
+#include "chip.hpp"
 // TODO: insert other definitions and declarations here
 const uint32_t OscRateIn = 12000000;
 const uint32_t RTCOscRateIn = 32768;
 
 int main(void) {
-
-	auto sel = Driver::Serial({0, 13}, {0, 12}, 0);
-    // TODO: insert code here
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-    }
     return 0 ;
 }
